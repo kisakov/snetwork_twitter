c84a1cb2c503f6b0beff708a0f8b68cea15f51af
kisakov <isakov90@gmail.com>
2014-07-20 22:24:39 +0400

    add TwServer to lib

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'twitter'
 gem 'tweetstream'
+gem 'logger'
 
 group :development do
   gem 'spring'
---
+++ lib/tw_server.rb
@@ -0,0 +1,24 @@
+class TwServer
+	def initialize(accounts)
+		@accounts, @logger = accounts, LoggerHelper.new("tw_server")
+		@logger.both "TwServer initiated"
+	end
+
+	def run
+		@logger.both "Running TweetStream client instance for #{@accounts.size} accounts"
+		Ts.follow(@accounts) do |status|
+		  receive_status(status)
+		end
+	end
+
+private
+
+	def receive_status(status)
+		@logger.file "========================"
+		@logger.both "Adding tweet to db from user_id: #{status.user.id}"
+		@logger.out "#{status.text}"
+
+		@logger.both "Tweet added"
+		@logger.file "========================"
+	end
+end
